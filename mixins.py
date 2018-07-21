3c21087775ab4092e299854bb460b1f1afe9ee78
shenwenlong <dalong5270@163.com>
2018-07-21 17:15:00 +0800

    fix

--- mixins.py
+++ mixins.py
@@ -19,7 +19,7 @@ class ExportModelMixin(object):
     def get_excel_name(self):
         if self.export_class.excel_name:
             return self.export_class.excel_name
-        return "Xxxxx"
+        raise ValueError
 
     def render_content(self):
         queryset = self.filter_queryset(self.get_queryset())
@@ -31,7 +31,6 @@ class ExportModelMixin(object):
             data.append([item.get(column) for column in  header])
         sheet_prd = self.wb.add_sheet('sheet1')
         for row_index, row in enumerate(data):
-            print(row)
             for column_index, column in enumerate(row):
                 sheet_prd.write(row_index, column_index, column)
         output = BytesIO()
@@ -39,12 +38,31 @@ class ExportModelMixin(object):
         output.seek(0)
         self.response.write(output.getvalue())
 
-
+# 例子
 class SalesExport:
+    # 表格的列 有序
     header = ["id","name", "period", "valid_order_count", "status", "started_at", "ended_at"]
+    # 表格名称 必写
     excel_name = "秒杀周期"
-
+    # 字段对应的表头名字, 非必写
     labels = {
         'name': '名字',
         'period': '周期'
     }
+
+
+class SalesActivityScheduleViewSet(ExportModelMixin,
+                                   GenericViewSet):
+    """
+    秒杀预设周期
+    """
+    queryset = SalesActivitySchedule.objects.all()
+    serializer_class = SalesActivityScheduleListSerializer
+    # 必写
+    export_class = SalesExport
+
+    def get_serializer_class(self):
+        if self.action == "export":
+            return SalesActivityScheduleListSerializer
+        return self.serializer_class
+
